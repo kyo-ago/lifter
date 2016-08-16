efbbd58687ea9dbf6184dfb4154474e41bca3e41
kyo-ago <kyo.ago@gmail.com>
2016-08-16 18:25:00 +0900

    add auto responder

--- src/domain/proxy/proxy-service.ts
+++ src/domain/proxy/proxy-service.ts
@@ -20,21 +20,26 @@ export class ProxyService {
     }
 
     createServer() {
-        proxy.onError(function(ctx: any, err: any) {
+        proxy.onError((ctx: any, err: any) => {
             console.error('proxy error:', err);
         });
 
-        proxy.onRequest(function(ctx: any, callback: any) {
-            if (ctx.clientToProxyRequest.headers.host == 'www.google.com'
-                && ctx.clientToProxyRequest.url.indexOf('/search') == 0) {
-                ctx.use(Proxy.gunzip);
+        proxy.onRequest((ctx: any, callback: any) => {
+            let encrypted = ctx.clientToProxyRequest.client.encrypted;
+            let host = ctx.clientToProxyRequest.headers.host;
+            let url = ctx.clientToProxyRequest.url;
+            let href = `http${encrypted ? `s` : ``}://${host}${url}`;
+            let reqestUrl = NodeUrl.parse(href);
 
-                ctx.onResponseData(function(ctx: any, chunk: any, callback: any) {
-                    chunk = new Buffer(chunk.toString().replace(/<h3.*?<\/h3>/g, '<h3>Pwned!</h3>'));
-                    return callback(null, chunk);
+            this.autoResponderEntryRepository.findMatchEntry(reqestUrl.pathname).then((result) => {
+                if (!result) {
+                    return callback();
+                }
+                ctx.proxyToClientResponse.writeHead(200, result.getHeader());
+                result.getBody().then((body) => {
+                    ctx.proxyToClientResponse.end(body);
                 });
-            }
-            return callback();
+            });
         });
 
         proxy.listen({
