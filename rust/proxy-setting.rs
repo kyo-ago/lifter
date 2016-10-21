03584f9464a1f328a53cea605ca265c4352ef047
kyo-ago <kyo.ago@gmail.com>
2016-10-21 23:34:28 +0900

    tmp

--- rust/proxy-setting.rs
+++ rust/proxy-setting.rs
@@ -3,23 +3,33 @@ use std::process::Command;
 
 fn main() {
     use std::os::unix::process::CommandExt;
-    let mut cmd = build_command();
-    cmd.uid(0);
-    let output = match cmd.output() {
+    let mut command = Command::new("/usr/sbin/networksetup");
+
+    let mut args = env::args();
+    args.next();
+
+    let param = args.next().unwrap();
+    if param == "-setwebproxy" {
+    } else if param == "-setsecurewebproxy" {
+    } else if param == "-setwebproxystate" {
+    } else if param == "-setsecurewebproxystate" {
+    } else {
+        panic!("Unknown parameter: {}", param);
+    }
+//    match param {
+//        "-setwebproxy" | "-setsecurewebproxy" | "-setwebproxystate" | "-setsecurewebproxystate" => "",
+//        _ => panic!("Unknown parameter: {}", param),
+//    }
+    command.arg(param);
+    for arg in args {
+        command.arg(arg);
+    }
+
+    command.uid(0);
+    let output = match command.output() {
         Ok(p) => p,
         Err(e) => panic!("Failed to execute: {}", e),
     };
-    print!("{}", String::from_utf8_lossy(output.stdout.as_slice()));
-}
 
-fn build_command() -> Command {
-    let mut command = Command::new("/usr/sbin/networksetup");
-    let args: Vec<_> = env::args().collect();
-    if args.len() > 1 {
-        for arg in args.iter() {
-            command.arg(arg[1]);
-            print!("{}", arg[1]);
-        }
-    }
-    command
+    print!("{}", String::from_utf8_lossy(output.stdout.as_slice()));
 }
