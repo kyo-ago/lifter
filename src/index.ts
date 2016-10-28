8d314e168184a082741187b0f999133b63ca4410
kyo-ago <kyo.ago@gmail.com>
2016-10-28 13:53:53 +0900

    tmp

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,31 @@
 import * as electron from "electron";
+const storage = require('electron-json-storage');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
 let mainWindow: any = null;
 
-let createWindow  = () => {
-    mainWindow = new BrowserWindow({width: 800, height: 600});
-    mainWindow.loadURL(`file://${__dirname}/index.html`);
-    mainWindow.webContents.openDevTools();
-    mainWindow.on('closed', () => {
-        mainWindow = null;
+let createWindow = () => {
+    debugger;
+    storage.get('windowBounds', (error: any, size: any) => {
+        if (error) throw error;
+
+        let param = Object.assign({
+            width: 600,
+            height: 300,
+        }, size, {
+            'min-width': 500,
+            'min-height': 200,
+            'accept-first-mouse': true,
+        });
+        mainWindow = new BrowserWindow(param);
+        mainWindow.loadURL(`file://${__dirname}/index.html`);
+        mainWindow.on('close', function() {
+            storage.set('windowBounds', mainWindow.getBounds());
+        });
+        mainWindow.on('closed', () => {
+            mainWindow = null;
+        });
     });
 };
 app.on('ready', createWindow);
