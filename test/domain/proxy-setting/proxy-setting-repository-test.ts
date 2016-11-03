eb1ebdc2b53236c9bba5c20ba392e0966a56a90a
kyo-ago <kyo.ago@gmail.com>
2016-11-03 22:18:33 +0900

    tmp

--- test/domain/proxy-setting/proxy-setting-repository-test.ts
+++ test/domain/proxy-setting/proxy-setting-repository-test.ts
@@ -1,36 +1,52 @@
 import 'mocha';
 import {} from 'node';
-import {PROXY_SETTING_COMMAND} from "../../../src/domain/settings";
-import {ProxySettingRepository} from "../../../src/domain/proxy-setting/proxy-setting-repository";
 
-const Path     = require('path');
-const assert   = require('assert');
 const fakeExec = require('fake-exec');
-const mockFs   = require('mock-fs');
-
-describe.only('ProxySettingRepository', () => {
-    afterEach(() => {
-        fakeExec.clear();
-        mockFs.restore();
-    });
-    it('get proxySettingRepository.getProxySetting', () => {
-        fakeExec('/usr/sbin/networksetup -listallnetworkservices', `An asterisk (*) denotes that a network service is disabled.
-Thunderbolt Ethernet
+let devices = `Thunderbolt Ethernet
 Apple USB Ethernet Adapter
 Wi-Fi
 Bluetooth PAN
-Thunderbolt Bridge
+Thunderbolt Bridge`;
+fakeExec('/usr/sbin/networksetup -listallnetworkservices', `An asterisk (*) denotes that a network service is disabled.
+${devices}
 `);
-        mockFs({
-            [PROXY_SETTING_COMMAND]: mockFs.file({
-                mode: parseInt('0666', 8),
-                uid: 500,
-                gid: 500,
-            }),
+
+import {PROXY_SETTING_COMMAND} from "../../../src/domain/settings";
+import {ProxySettingRepository} from "../../../src/domain/proxy-setting/proxy-setting-repository";
+
+const Path   = require('path');
+const assert = require('assert');
+const mockFs = require('mock-fs');
+
+let execMockFs = (uidgid: number) => {
+    mockFs({
+        [PROXY_SETTING_COMMAND]: mockFs.file({
+            mode: parseInt('0755', 8),
+            uid: uidgid,
+            gid: uidgid,
+        }),
+    });
+};
+
+describe('ProxySettingRepository', () => {
+    afterEach(() => {
+        mockFs.restore();
+    });
+    it('proxySettingRepository.getProxySetting isGranted', () => {
+        execMockFs(0);
+        return new ProxySettingRepository().getProxySetting().then((entity) => {
+            assert(entity.isGranted);
         });
-        let proxySettingRepository = new ProxySettingRepository();
-        return proxySettingRepository.getProxySetting().then((a) => {
-            console.log(a)
+    });
+    it('proxySettingRepository.getProxySetting !isGranted', () => {
+        execMockFs(500);
+        return new ProxySettingRepository().getProxySetting().then((entity) => {
+            assert(!entity.isGranted);
+        });
+    });
+    it('proxySettingRepository.getProxySetting devices', () => {
+        return new ProxySettingRepository().getProxySetting().then((entity) => {
+            assert(entity.devices.join("\n") === devices);
         });
     });
 });
