118714d19c3f89fd3cddf4953c6fa5f38e15f583
kyo-ago <kyo.ago@gmail.com>
2016-11-21 13:59:23 +0900

    tmp

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,27 @@
 import * as electron from "electron";
-const storage = require('electron-json-storage');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+const windowStateKeeper = require('electron-window-state');
 
 let mainWindow: any = null;
 
 let createWindow = () => {
-    debugger;
-    storage.get('windowBounds', (error: any, size: any) => {
-        if (error) throw error;
-
-        let param = Object.assign({
-            width: 600,
-            height: 300,
-        }, size, {
-            'min-width': 500,
-            'min-height': 200,
-            'accept-first-mouse': true,
-        });
-        mainWindow = new BrowserWindow(param);
-        mainWindow.loadURL(`file://${__dirname}/index.html`);
-        mainWindow.on('close', function() {
-            storage.set('windowBounds', mainWindow.getBounds());
-        });
-        mainWindow.on('closed', () => {
-            mainWindow = null;
-        });
+    let mainWindowState = windowStateKeeper({
+        defaultWidth: 1000,
+        defaultHeight: 800
     });
+
+    mainWindow = new BrowserWindow({
+        'x': mainWindowState.x,
+        'y': mainWindowState.y,
+        'width': mainWindowState.width,
+        'height': mainWindowState.height
+    });
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+    mainWindowState.manage(mainWindow);
 };
 app.on('ready', createWindow);
 if (process.platform !== 'darwin') {
