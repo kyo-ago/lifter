c369493661643d75a666d475019ed0ae94c83e31
kyo-ago <kyo.ago@gmail.com>
2017-01-12 13:59:35 +0900

    add menu

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,1 +1,166 @@
 import "./ui/render";
+import {app, Menu} from "electron";
+
+const template: any = [
+    {
+        label: 'Proxy',
+        submenu: [
+            {
+                role: 'undo'
+            },
+            {
+                role: 'redo'
+            },
+            {
+                type: 'separator'
+            },
+            {
+                role: 'cut'
+            },
+            {
+                role: 'copy'
+            },
+            {
+                role: 'paste'
+            },
+            {
+                role: 'pasteandmatchstyle'
+            },
+            {
+                role: 'delete'
+            },
+            {
+                role: 'selectall'
+            }
+        ]
+    },
+    {
+        label: 'View',
+        submenu: [
+            {
+                role: 'reload'
+            },
+            {
+                role: 'toggledevtools'
+            },
+            {
+                type: 'separator'
+            },
+            {
+                role: 'resetzoom'
+            },
+            {
+                role: 'zoomin'
+            },
+            {
+                role: 'zoomout'
+            },
+            {
+                type: 'separator'
+            },
+            {
+                role: 'togglefullscreen'
+            }
+        ]
+    },
+    {
+        role: 'window',
+        submenu: [
+            {
+                role: 'minimize'
+            },
+            {
+                role: 'close'
+            }
+        ]
+    },
+    {
+        role: 'help',
+        submenu: [
+            {
+                label: 'Learn More',
+                click () { require('electron').shell.openExternal('http://electron.atom.io') }
+            }
+        ]
+    }
+]
+
+if (process.platform === 'darwin') {
+    template.unshift({
+                         label: app.getName(),
+                         submenu: [
+                             {
+                                 role: 'about'
+                             },
+                             {
+                                 type: 'separator'
+                             },
+                             {
+                                 role: 'services',
+                                 submenu: []
+                             },
+                             {
+                                 type: 'separator'
+                             },
+                             {
+                                 role: 'hide'
+                             },
+                             {
+                                 role: 'hideothers'
+                             },
+                             {
+                                 role: 'unhide'
+                             },
+                             {
+                                 type: 'separator'
+                             },
+                             {
+                                 role: 'quit'
+                             }
+                         ]
+                     })
+    // Edit menu.
+    template[1].submenu.push(
+        {
+            type: 'separator'
+        },
+        {
+            label: 'Speech',
+            submenu: [
+                {
+                    role: 'startspeaking'
+                },
+                {
+                    role: 'stopspeaking'
+                }
+            ]
+        }
+    )
+    // Window menu.
+    template[3].submenu = [
+        {
+            label: 'Close',
+            accelerator: 'CmdOrCtrl+W',
+            role: 'close'
+        },
+        {
+            label: 'Minimize',
+            accelerator: 'CmdOrCtrl+M',
+            role: 'minimize'
+        },
+        {
+            label: 'Zoom',
+            role: 'zoom'
+        },
+        {
+            type: 'separator'
+        },
+        {
+            label: 'Bring All to Front',
+            role: 'front'
+        }
+    ]
+}
+
+const menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
