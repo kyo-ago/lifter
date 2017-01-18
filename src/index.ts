b4f061323555ff5b31705735bb736ab62f2923d2
kyo-ago <kyo.ago@gmail.com>
2017-01-18 13:56:40 +0900

    tmp

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import {app, BrowserWindow} from "electron";
+import {app, BrowserWindow, Menu} from "electron";
 const windowStateKeeper = require('electron-window-state');
 
 let mainWindow: any = null;
@@ -21,3 +21,129 @@ if (process.platform !== 'darwin') {
     app.on('window-all-closed', () => app.quit());
 }
 app.on('activate', () => mainWindow || createWindow());
+
+
+
+
+const template: any = [
+    {
+        label: 'Proxy',
+        submenu: [
+            {
+                role: 'undo'
+            },
+        ]
+    },
+    {
+        label: 'View',
+        submenu: [
+            {
+                role: 'reload'
+            },
+            {
+                role: 'toggledevtools'
+            },
+            {
+                type: 'separator'
+            },
+            {
+                role: 'resetzoom'
+            },
+            {
+                role: 'zoomin'
+            },
+            {
+                role: 'zoomout'
+            },
+            {
+                type: 'separator'
+            },
+            {
+                role: 'togglefullscreen'
+            },
+        ],
+    },
+    {
+        role: 'window',
+        submenu: [
+            {
+                role: 'minimize'
+            },
+            {
+                role: 'close'
+            },
+        ],
+    },
+    {
+        role: 'help',
+        submenu: [
+            {
+                label: 'Learn More',
+                click () { require('electron').shell.openExternal('http://electron.atom.io') }
+            },
+        ],
+    },
+];
+
+if (process.platform === 'darwin') {
+    template.unshift({
+                         label  : app.getName(),
+                         submenu: [
+                             {
+                                 role: 'about'
+                             },
+                             {
+                                 type: 'separator'
+                             },
+                             {
+                                 role   : 'services',
+                                 submenu: []
+                             },
+                             {
+                                 type: 'separator'
+                             },
+                             {
+                                 role: 'hide'
+                             },
+                             {
+                                 role: 'hideothers'
+                             },
+                             {
+                                 role: 'unhide'
+                             },
+                             {
+                                 type: 'separator'
+                             },
+                             {
+                                 role: 'quit'
+                             },
+                         ],
+                     });
+    // Window menu.
+    template[3].submenu = [
+        {
+            label: 'Close',
+            accelerator: 'CmdOrCtrl+W',
+            role: 'close'
+        },
+        {
+            label: 'Minimize',
+            accelerator: 'CmdOrCtrl+M',
+            role: 'minimize'
+        },
+        {
+            label: 'Zoom',
+            role: 'zoom'
+        },
+        {
+            type: 'separator'
+        },
+        {
+            label: 'Bring All to Front',
+            role: 'front'
+        }
+    ]
+}
+
+const menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
