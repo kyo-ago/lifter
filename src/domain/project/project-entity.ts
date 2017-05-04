83ac92dce2ca335839f828362ae09532f4dd9f87
kyo-ago <kyo.ago@gmail.com>
2017-05-05 02:49:09 +0900

    add responders

--- src/domain/project/project-entity.ts
+++ src/domain/project/project-entity.ts
@@ -1,5 +1,12 @@
 import {BaseEntity} from "../base/base-entity";
 import {ProjectIdentity} from "./project-identity";
+import {ProjectName} from "./value-objects/project-name";
+import {AutoResponderEntryBaseEntity} from "../auto-responder/entry/base/auto-responder-entry-base-entity";
 
 export class ProjectEntity extends BaseEntity<ProjectIdentity> {
+    private name: ProjectName;
+    private autoResponders: AutoResponderEntryBaseEntity[];
+
+    addResponder() {}
+    deleteResponder() {}
 }
