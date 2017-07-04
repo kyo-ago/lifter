20c229e80b5e3331785128ae54fe53988776559b
kyo-ago <kyo.ago@gmail.com>
2017-07-05 01:45:03 +0900

    tmp

---
+++ src/ui/rewrite-rule-setting-window/components/toolbar/open-rewrite-rule-setting-window-box.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import {GlobalProps} from "../index";
+
+export class OpenRewriteRuleSettingWindowBox extends React.Component<GlobalProps, {}> {
+    onClick() {
+        this.props.openRewriteRuleSettingWindow();
+    }
+
+    render() {
+        return (
+            <button className="btn btn-default" title="Open rewrite rule setting window" onClick={this.onClick.bind(this)}>
+                <span className="icon icon-export"></span>
+            </button>
+        );
+    }
+}
