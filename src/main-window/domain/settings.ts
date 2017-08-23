63c139d94630ecc6df4b005e46e86bbff312defe
kyo-ago <kyo.ago@gmail.com>
2017-08-23 12:07:45 +0900

    Clean up package.json

--- src/main-window/domain/settings.ts
+++ src/main-window/domain/settings.ts
@@ -1,12 +1,10 @@
 import * as fs from "fs";
 
 export const PROXY_PORT = 8888;
-export const APPLICATION_NAME = require('../../../package.json').build.productName;
+export const APPLICATION_NAME = 'Lifter Proxy';
 export const NETWORK_SETUP_COMMAND = '/usr/sbin/networksetup';
 export const NETWORK_HOST_NAME = 'localhost';
 export const SECURITY_COMMAND = '/usr/bin/security';
-export const SETTING_FILE_NAME = 'lifterProxy.conf.js';
-export const DATA_STORE_FILENAME = 'lifterProxy.save.db';
 export const HTTP_SSL_CA_DIR_PATH = '.http-mitm-proxy';
 
 let production = __dirname.replace(/app\.asar\/.+/, `app.asar.unpacked/node_modules/networksetup-proxy/rust/proxy-setting`);
