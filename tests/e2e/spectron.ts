d9485389221a4e342bdd38d469dc6e9e2da45c98
kyo-ago <kyo.ago@gmail.com>
2017-10-19 15:36:11 +0900

    add base code

--- tests/e2e/spectron.ts
+++ tests/e2e/spectron.ts
@@ -1,10 +1,27 @@
 import {Application} from "spectron";
+import * as assert from "assert";
 import * as Path from "path";
 
-let ext = process.platform === 'win32' ? '.cmd' : '';
-export const TestApplication = new Application({
-    path: `${Path.join('.', 'node_modules', '.bin', 'electron')}${ext}`,
-    args: [Path.join('.')]
-});
+describe('application launch', function () {
+    this.timeout(10000);
 
-afterAll(() => TestApplication.stop());
+    beforeEach(function () {
+        this.app = new Application({
+            path: `${Path.join('.', 'node_modules', '.bin', 'electron')}`,
+            args: [Path.join('.')],
+        });
+        return this.app.start();
+    });
+
+    afterEach(function () {
+        if (this.app && this.app.isRunning()) {
+            return this.app.stop();
+        }
+    });
+
+    it('shows an initial window', function () {
+        return this.app.client.getWindowCount().then(function (count) {
+            assert.equal(count, 1);
+        });
+    });
+});
