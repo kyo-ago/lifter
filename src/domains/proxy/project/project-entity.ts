1075d2298a9a60150400cd23c6f82c57070a3fd5
kyo_ago <kyo.ago@gmail.com>
2017-10-21 22:12:41 +0900

    Add e2e tests (#28)
    * add base code
    
    * smoke test
    
    * add test

--- src/domains/proxy/project/project-entity.ts
+++ src/domains/proxy/project/project-entity.ts
@@ -1,28 +1,30 @@
-import {Option} from 'monapt';
+import {DataStoreOptions} from "nedb";
 import {BaseEntity} from '../../share/base/base-entity';
 import {ProjectIdentity} from './project-identity';
 import {ProjectBaseDir} from "./value-objects/project-base-dir";
 import {ProjectName} from './value-objects/project-name';
 
 export class ProjectEntity extends BaseEntity<ProjectIdentity> {
-    public path: Option<ProjectBaseDir>;
-
     constructor(
         projectIdentity: ProjectIdentity,
         private name: ProjectName,
-        path?: ProjectBaseDir,
+        public baseDir: ProjectBaseDir,
     ) {
         super(projectIdentity);
-        this.path = Option(path);
     }
     get json() {
         return {
             id: this.id,
             name: this.name.value,
-            baseDir: this.path.match({
-                Some: (path) => path.value,
-                None: () => null,
-            })
+            baseDir: this.baseDir.value,
         }
+    }
+
+    getDataStoreOptions(name: string): DataStoreOptions {
+        return process.env.NODE_ENV === 'test' ? {
+            inMemoryOnly: true,
+        } : {
+            filename: `${this.baseDir.value}/${name}.nedb`,
+        };
     }
 }
---
+++ tests/e2e/index.spec.ts
@@ -0,0 +1,30 @@
+import {Application} from "spectron";
+import * as assert from "assert";
+import * as Path from "path";
+
+describe('application launch', () => {
+    let application: Application;
+    beforeAll(() => {
+        application = new Application({
+            path: `${Path.join('.', 'node_modules', '.bin', 'electron')}`,
+            args: [Path.join('.')],
+        });
+        return application.start();
+    });
+
+    afterAll(() => {
+        if (application && application.isRunning()) {
+            return application.stop();
+        }
+    });
+
+    it('shows an initial window', async () => {
+        let count = await application.client.getWindowCount();
+        assert.equal(count, 1);
+    });
+
+    it('get text', async () => {
+        let text = await (<any>application.client).getText('body');
+        assert(text);
+    });
+});
