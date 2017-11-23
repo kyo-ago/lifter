a3c19d2ef55adaa12389dd79d486a1d15a19eb23
kyo-ago <kyo.ago@gmail.com>
2017-11-23 23:06:35 +0900

    fix css

--- packages/lifter-app/src/renderer/index.ts
+++ packages/lifter-app/src/renderer/index.ts
@@ -2,6 +2,13 @@ import { render as appRender } from "../process/renders/main/ui/render";
 import { render as proxyBypassDomainSettingWindowRender } from "../process/renders/proxy-bypass-domain-setting/ui/render";
 import { render as rewriteRuleSettingWindowRender } from "../process/renders/rewrite-rule-setting/ui/render";
 
+function requireAll(r: any) {
+    r.keys().forEach(r);
+}
+
+requireAll(require.context("./", true, /\.css$/));
+require("./index.css");
+
 document.addEventListener("DOMContentLoaded", () => {
     [
         {
