e1b03a65df3120c8182fb1919a47778e6469d806
kyo-ago <kyo.ago@gmail.com>
2017-12-07 14:00:57 +0900

    refactoring

--- packages/lifter-app/src/main/index.ts
+++ packages/lifter-app/src/main/index.ts
@@ -1,20 +1,14 @@
 import { app } from "electron";
-import { ProjectFactory } from "../domains/proxy/project/lifecycle/project-factory";
-import { Application } from "../../../lifter-main/src/application/application";
-import { LifecycleContextService } from "../../../lifter-main/src/application/lifecycle-context-service";
-import { REPOSITORY_BASE_DIR_PATH } from "../settings";
+import {REPOSITORY_BASE_DIR_PATH, USER_DATA_PATH} from "../settings";
 import {createApplication} from '@kyo-ago/lifter-main';
 
-let application = createApplication();
+let application = createApplication(REPOSITORY_BASE_DIR_PATH, USER_DATA_PATH);
 
 app.on("window-all-closed", async () => {
     await application.quit();
     app.quit();
 });
 app.on("activate", () => application.createMainWindow());
-
-let projectEntity = new ProjectFactory().create(REPOSITORY_BASE_DIR_PATH);
-let application = new Application(projectEntity, new LifecycleContextService(projectEntity));
 
 Promise.all([application.load(), new Promise(resolve => app.on("ready", resolve))])
     .then(() => {
@@ -23,4 +17,5 @@ Promise.all([application.load(), new Promise(resolve => app.on("ready", resolve)
     })
     .catch((...args: any[]) => {
         console.error(args);
-    });
+    })
+;
--- packages/lifter-app/src/settings.ts
+++ packages/lifter-app/src/settings.ts
@@ -5,6 +5,7 @@ let userDataPath = app ? app.getPath("userData") : ".";
 
 export const NETWORK_SETUP_COMMAND = "/usr/sbin/networksetup";
 export const SECURITY_COMMAND = "/usr/bin/security";
+export const USER_DATA_PATH = userDataPath;
 export const REPOSITORY_BASE_DIR_PATH = `${userDataPath}/Repositories`;
 export const WINDOW_STATE_DIR = "WindowStates/";
 export const WindowManagerInit = {
