e191979f405c7ce186a1a437951569f0b3f3fc7f
kyo-ago <kyo.ago@gmail.com>
2018-03-31 18:07:54 +0900

    add returns to application mock apis

--- packages/lifter-app/src/renderer/application/application.mock.ts
+++ packages/lifter-app/src/renderer/application/application.mock.ts
@@ -1,10 +1,43 @@
 import * as sinon from "sinon";
+import { ApplicationMainStateJSON } from "../../main/window-manager";
 import { Application } from "./application";
 
 let sandbox = sinon.createSandbox();
 
 let applicationMock = sandbox.createStubInstance(Application);
-applicationMock.getCurrentState.callsFake(() => ({}));
+applicationMock.getCurrentState.callsFake((): ApplicationMainStateJSON => ({
+    autoResponderEntries: [],
+    clientRequestEntries: [],
+    proxyBypassDomainEntries: [],
+    rewriteRuleEntries: [],
+    certificateState:  "missing",
+    proxySettingStatus: "Off",
+    proxyCommandGrantStatus: "Off",
+    noAutoEnableProxySetting: false,
+    noPacFileProxySetting: false,
+}));
+applicationMock.addDropFiles.resolves([]);
+applicationMock.selectDialogEntry.resolves([]);
+applicationMock.fetchAutoResponderEntities.resolves([]);
+applicationMock.deleteAutoResponderEntities.resolves();
+applicationMock.changeCertificateStatus.resolves("installed");
+applicationMock.changeProxySettingStatus.resolves("Off");
+applicationMock.changeProxyCommandGrantStatus.resolves("Off");
+applicationMock.changeNoAutoEnableProxySetting.resolves(true);
+applicationMock.changeNoPacFileProxySetting.resolves(true);
+applicationMock.getProxyBypassDomains.resolves([]);
+applicationMock.saveProxyBypassDomains.resolves(undefined);
+applicationMock.getRewriteRules.resolves([]);
+applicationMock.addRewriteRule.callsFake((url: string) => {
+    return {
+        id: 1,
+        url: url,
+        modifier: [],
+    };
+});
+applicationMock.deleteRewriteRules.resolves(undefined);
+applicationMock.addRewriteRuleModifier.resolves(undefined);
+applicationMock.deleteRewriteRuleModifiers.resolves(undefined);
 
 afterEach(() => {
     sandbox.resetHistory();
--- packages/lifter-app/src/renderer/application/application.ts
+++ packages/lifter-app/src/renderer/application/application.ts
@@ -20,7 +20,7 @@ export class Application {
     ) {
     }
 
-    load() {
+    load(): void {
         this.contextMenuService.bind();
     }
 
