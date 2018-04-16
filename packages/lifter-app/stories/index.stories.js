88736e2a056290f2837dff0bf14309e553af85fb
kyo-ago <kyo.ago@gmail.com>
2018-04-16 21:40:10 +0900

    storybook build successful

--- packages/lifter-app/stories/index.stories.js
+++ packages/lifter-app/stories/index.stories.js
@@ -1,47 +1,43 @@
-import { storiesOf } from '@storybook/vue';
-// import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
-
-import App from '../src/renderer/components/app';
-import Divider from '../src/renderer/components/li-header/divider';
-import LeftToolbar from '../src/renderer/components/li-header/left-toolbar';
-import LiHeader from '../src/renderer/components/li-header/li-header';
-import RightToolbar from '../src/renderer/components/li-header/right-toolbar';
-import TabContents from '../src/renderer/components/li-header/tab-contents';
+import { storiesOf } from "@storybook/vue";
+import App from "../src/renderer/components/app";
+import LiHeader from "../src/renderer/components/li-header/li-header";
+import LiMain from "../src/renderer/components/li-main/li-main";
+import LiDialog from "../src/renderer/components/li-dialog/li-dialog";
 import { getStore } from "../src/renderer/store";
 import { ApplicationMock } from "../src/renderer/application/application.mock";
+import { getLocale, messages } from "../src/renderer/messages";
+import VueI18n from "vue-i18n";
+// import { action } from '@storybook/addon-actions';
+// import { linkTo } from "@storybook/addon-links";
 
-storiesOf('App', module).add('app', () => ({
-    components: { App },
-    template: '<App />',
-    methods: { action: linkTo('li-header') },
-    store: getStore(ApplicationMock),
-}));
+let i18n = new VueI18n({
+    locale: getLocale(navigator),
+    messages,
+});
 
-storiesOf('li-header', module)
+storiesOf('App', module)
+    .add('app', () => ({
+        components: { App },
+        template: '<App />',
+        store: getStore(ApplicationMock),
+        i18n,
+    }))
     .add('li-header', () => ({
         components: { LiHeader },
         template: '<li-header />',
         store: getStore(ApplicationMock),
+        i18n,
     }))
-    .add('left-toolbar', () => ({
-        components: { LeftToolbar },
-        template: '<left-toolbar />',
+    .add('li-main', () => ({
+        components: { LiMain },
+        template: '<li-main />',
         store: getStore(ApplicationMock),
+        i18n,
     }))
-    .add('right-toolbar', () => ({
-        components: { RightToolbar },
-        template: '<right-toolbar />',
+    .add('li-dialog', () => ({
+        components: { LiDialog },
+        template: '<li-dialog />',
         store: getStore(ApplicationMock),
-    }))
-    .add('tab-contents', () => ({
-        components: { TabContents },
-        template: '<tab-contents />',
-        store: getStore(ApplicationMock),
-    }))
-    .add('divider', () => ({
-        components: { Divider },
-        template: '<divider />',
-        store: getStore(ApplicationMock),
+        i18n,
     }))
 ;
--- packages/lifter-app/storybook/webpack.config.js
+++ packages/lifter-app/storybook/webpack.config.js
@@ -1,45 +1,71 @@
-let path = require("path");
-let testConfig = require("../test/webpack.config");
+const path = require("path");
+const webpackMerge = require('webpack-merge');
+const testConfig = require("../test/webpack.config");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = (storybookBaseConfig) => {
-    storybookBaseConfig.module.rules = storybookBaseConfig.module.rules.map((rule) => {
-        return testConfig.module.rules.find(_ => _.test.toString() === rule.test.toString()) || rule;
-    }).concat(testConfig.module.rules.filter((rule) => {
-        return !storybookBaseConfig.module.rules.find(_ => _.test.toString() === rule.test.toString());
-    }));
-    storybookBaseConfig.module.rules.filter(_ => _.test.test(".vue")).forEach((rule) => {
-        rule.use[0].options.loaders.ts = {
-            loader: 'ts-loader',
-            options: {
-                appendTsSuffixTo: [/\.vue$/],
-                happyPackMode: true,
+    storybookBaseConfig.module.rules = storybookBaseConfig.module.rules.filter((item) => {
+        return !(item.test.toString().match(/\.vue\W/));
+    });
+    delete testConfig.externals;
+    let resultConfig = webpackMerge.smart(
+        storybookBaseConfig,
+        testConfig,
+        {
+            module: {
+                rules: [
+                    {
+                        test: /\.vue$/,
+                        use: [
+                            {
+                                loader: "vue-loader",
+                                options: {
+                                    extractCSS: true,
+                                    loaders: {
+                                        ts: {
+                                            loader: 'ts-loader',
+                                            options: {
+                                                appendTsSuffixTo: [/\.vue$/],
+                                                happyPackMode: true,
+                                            },
+                                        },
+                                        scss: 'vue-style-loader!css-loader!sass-loader',
+                                        sass: 'vue-style-loader!css-loader!sass-loader?indentedSyntax',
+                                    },
+                                },
+                            },
+                        ],
+                    },
+                    {
+                        test: /\.css$/,
+                        use: ExtractTextPlugin.extract({
+                            fallback: "style-loader",
+                            use: "css-loader"
+                        }),
+                    },
+                    {
+                        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+                        use: {
+                            loader: "url-loader",
+                            options: {
+                                limit: 10240,
+                                name: "fonts/[name]--[folder].[ext]"
+                            }
+                        }
+                    },
+                ],
             },
-        };
-        rule.use[0].options.loaders.scss = 'vue-style-loader!css-loader!sass-loader';
-        rule.use[0].options.loaders.sass = 'vue-style-loader!css-loader!sass-loader?indentedSyntax';
-    });
-    storybookBaseConfig.module.rules.push({
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-    });
-    storybookBaseConfig.module.rules.push({
-        test: /\.(woff|woff2|eot|ttf|svg)$/,
-        loader: 'file-loader?name=../font/[name].[ext]',
-    });
-
-    storybookBaseConfig.resolve = testConfig.resolve;
-    Object.assign(storybookBaseConfig.resolve.alias, {
-        "electron": path.resolve(__dirname, '../mocks/electron'),
-        "electron-ipc": path.resolve(__dirname, '../mocks/electron-ipc'),
-        "electron-context-menu": path.resolve(__dirname, '../mocks/electron-context-menu'),
-    });
-
-    storybookBaseConfig.node = testConfig.node;
-    storybookBaseConfig.devtool = testConfig.devtool;
-    storybookBaseConfig.devServer = testConfig.devServer;
-    // storybookBaseConfig.externals = testConfig.externals;
-    storybookBaseConfig.performance = testConfig.performance;
-    storybookBaseConfig.plugins = storybookBaseConfig.plugins.concat(testConfig.plugins);
-
-    return storybookBaseConfig;
+            plugins: [
+                new ExtractTextPlugin("styles.css"),
+            ],
+            resolve: {
+                alias: {
+                    "electron": path.resolve(__dirname, '../mocks/electron'),
+                    "electron-ipc": path.resolve(__dirname, '../mocks/electron-ipc'),
+                    "electron-context-menu": path.resolve(__dirname, '../mocks/electron-context-menu'),
+                },
+            },
+        }
+    );
+    return resultConfig;
 };
