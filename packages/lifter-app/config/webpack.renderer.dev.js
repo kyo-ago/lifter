8725bf6911c49062d30cdde46cf406288931f1e1
kyo-ago <kyo.ago@gmail.com>
2018-05-05 16:43:53 +0900

    fix: webpack.conf.js for development environment

---
+++ packages/lifter-app/config/webpack.renderer.dev.js
@@ -0,0 +1,19 @@
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                use: [
+                    {
+                        loader: "vue-loader",
+                        options: {
+                            loaders: {
+                                i18n: "@kazupon/vue-i18n-loader",
+                            },
+                        },
+                    },
+                ],
+            }
+        ]
+    }
+};
